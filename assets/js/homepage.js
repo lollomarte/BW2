5ad69d34579609490c5ee3aa897a2afc4ae760f6
Lorenzo Martegiani <lorenzomartegiani@gmail.com>
2023-02-01 14:17:10 +0100

    papap

---
+++ assets/js/homepage.js
@@ -0,0 +1,19 @@
+
+async function homeAlbum() {
+    const fetchSec2 = await fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=origins");
+    const HomeFetch = await fetchSec2.json();
+    
+    const homeimg = document.querySelector(".homeimg");
+    homeimg.src = HomeFetch.data[0].album.cover_big;
+    const AlbumTitle = document.querySelector(".TitoloCanzone");
+    AlbumTitle.innerText = HomeFetch.data[0].title;
+    const ArtistName = document.querySelector(".ArtistName");
+    ArtistName.innerText = HomeFetch.data[0].artist.name;
+}
+
+
+
+window.onload = homeAlbum()
+
+
+
